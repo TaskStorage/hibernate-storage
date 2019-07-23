6c62f7a70e1c8428804cd42a1f79ce5c62ece214
Konstantin <konsttask@gmail.com>
2019-07-23 22:50:47 +0300

    initial commit

---
+++ src/main/java/com/taskstorage/HibernateUtil.java
@@ -0,0 +1,25 @@
+package com.taskstorage;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            // Create SessionFactory from hibernate.cfg.xml
+            return new Configuration()
+                    .configure()
+                    .buildSessionFactory();
+        } catch (Throwable ex) {
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
---
+++ src/main/java/com/taskstorage/Task.java
@@ -0,0 +1,59 @@
+package com.taskstorage;
+
+import javax.persistence.*;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String description;
+    private String content;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    public Task() {
+    }
+
+    public Task(String description, String content, User author) {
+        this.description = description;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+}
